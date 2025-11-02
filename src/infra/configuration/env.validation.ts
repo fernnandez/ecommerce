e267f6b50519f02a7814a4fa5db236e511fb944e
Angelo Fernandes <angelo.fernandes.dev@gmail.com>
2025-11-02 00:02:35 -0300

    fix: remove redis env

--- src/infra/configuration/env.validation.ts
+++ src/infra/configuration/env.validation.ts
@@ -4,8 +4,6 @@ export default Joi.object({
   NODE_ENV: Joi.string().valid('development', 'production', 'test'),
   DATABASE_URL: Joi.string().required(),
   PORT: Joi.number(),
-  REDIS_HOST: Joi.string().required(),
-  REDIS_PORT: Joi.string().required(),
   JWT_SECRET: Joi.string().required(),
   JWT_EXPIRES_IN: Joi.string().default('24h'),
   JWT_REFRESH_SECRET: Joi.string().optional(),
